717dab1a64b8f0ceacf2cbd917389f01af34ff42
alex-mrk <gamerino@pichincha.com>
2021-03-23 00:24:28 -0500

    Added a simple test

--- tienda_api/models.py
+++ tienda_api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserProfileManager(BaseUserManager):
 
     def create_user(self, email, name, password=None):
@@ -10,28 +11,29 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('Usuario debe tener un email')
         email = self.normalize_email(email)
-        user = self.model(email= email, name= name)
+        user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name , password):
-        user = self.create_user(email,name,password)
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using= self._db)
+        user.save(using=self._db)
         return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Modelo base de datos para usuarios en el sistema"""
-    email= models.EmailField(max_length=255,unique=True)
-    name= models.CharField(max_length=255)
-    is_active= models.BooleanField(default=True)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD= 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
@@ -42,3 +44,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    identification = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
--- tienda_api/tests.py
+++ tienda_api/tests.py
@@ -1,3 +1,17 @@
+import json
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from django.test import TestCase
+from tienda_api.models import Client
 
-# Create your tests here.
+
+class StoreTestCase(APITestCase):
+
+    def setUp(self):
+        Client.objects.create(identification="1720009057", name="Geovanny Alexander", lastname="Merino Romero",
+                              email="alexmerino67@gmail.com", phone="0995308851")
+
+    def test_validate_client(self):
+        student = Client.objects.get(name="Geovanny Alexander")
+        self.assertEqual(str(student.phone),"0995308851")
